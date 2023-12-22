7f937cbd2432f3dd3f93ed7a66b670751efce542
Axel Verdasca <axel.verdasca@numericable.fr>
2023-12-22 11:47:40 +0100

    test

--- assets/script.js
+++ assets/script.js
@@ -41,58 +41,58 @@ function nbrslide(){
 /*** Action lors du click sur l'une des deux flèches ***/
 flechegauche.addEventListener('click', function(){
 	numslide--;
-	nbrslide()
-	slide__dot()
-	slide__changement()
-	console.log(numslide)
+	nbrslide();
+	slide__dot();
+	slide__changement();
+	console.log(numslide);
 })
 
 flechedroite.addEventListener('click', function(){
 	numslide++;
-	nbrslide()
-	slide__dot()
-	slide__changement()
-	console.log(numslide)
+	nbrslide();
+	slide__dot();
+	slide__changement();
+	console.log(numslide);
 })
 
 /*** Affichage des points en fonction de la slide ***/
 function slide__dot() {
 	if(numslide === 0){
-		dot1.classList.add('dot_selected')
-		dot2.classList.remove('dot_selected')
-		dot3.classList.remove('dot_selected')
-		dot4.classList.remove('dot_selected')
+		dot1.classList.add('dot_selected');
+		dot2.classList.remove('dot_selected');
+		dot3.classList.remove('dot_selected');
+		dot4.classList.remove('dot_selected');
 	}
 	if(numslide === 1){
-		dot1.classList.remove('dot_selected')
-		dot2.classList.add('dot_selected')
-		dot3.classList.remove('dot_selected')
-		dot4.classList.remove('dot_selected')
+		dot1.classList.remove('dot_selected');
+		dot2.classList.add('dot_selected');
+		dot3.classList.remove('dot_selected');
+		dot4.classList.remove('dot_selected');
 	}
 	if(numslide === 2){
-		dot1.classList.remove('dot_selected')
-		dot2.classList.remove('dot_selected')
-		dot3.classList.add('dot_selected')
-		dot4.classList.remove('dot_selected')
+		dot1.classList.remove('dot_selected');
+		dot2.classList.remove('dot_selected');
+		dot3.classList.add('dot_selected');
+		dot4.classList.remove('dot_selected');
 	}
 	if(numslide === 3) {
-		dot1.classList.remove('dot_selected')
-		dot2.classList.remove('dot_selected')
-		dot3.classList.remove('dot_selected')
-		dot4.classList.add('dot_selected')
+		dot1.classList.remove('dot_selected');
+		dot2.classList.remove('dot_selected');
+		dot3.classList.remove('dot_selected');
+		dot4.classList.add('dot_selected');
 	}
 }
 
 /*** Changement de slide ***/
 const urlprefix= "./assets/images/slideshow/"
 function slide__changement(){
-	const imagebanner = document.getElementById('banner__image')
-	const text = document.getElementById('banner__text')
-	let image = slides[numslide].image
-	let tagLine = slides[numslide].tagLine
-	imagebanner.src = urlprefix + image
-	text.innerHTML=""
-	const newtag = document.createElement("p")
-	newtag.innerHTML = tagLine
-	text.appendChild(newtag)
+	const imagebanner = document.getElementById('banner__image');
+	const text = document.getElementById('banner__text');
+	let image = slides[numslide].image;
+	let tagLine = slides[numslide].tagLine;
+	imagebanner.src = urlprefix + image;
+	text.innerHTML="";
+	const newtag = document.createElement("p");
+	newtag.innerHTML = tagLine;
+	text.appendChild(newtag);
 }
